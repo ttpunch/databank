7ae1bfde2bdaa43c34c8e0aef62849e0b75cd63f
ttpunch <vinod418@gmail.com>
2025-02-08 11:42:42 +0530

    Add Tailwind CSS configuration and implement responsive design in the main page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,18 @@
-import type { Config } from 'tailwindcss'
- 
+import type { Config } from "tailwindcss";
+
 export default {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
- 
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
   plugins: [],
-} satisfies Config
+} satisfies Config;
