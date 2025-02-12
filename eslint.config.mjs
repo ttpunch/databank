a6fefab582bc4b94b57b3ea06a441dab3aa992d9
ttpunch <vinod418@gmail.com>
2025-02-12 18:18:23 +0530

    eslint1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,9 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off", // Disable the rule temporarily
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+       // Disable the rule temporarily
     },
   },
 ];
